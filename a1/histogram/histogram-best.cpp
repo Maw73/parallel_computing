fb390197ae53cfc0903bbeeee10ded9e15b7037d
Maria Mickonovek <andreia.lazarec@gmail.com>
2025-04-02 19:45:14 +0200

    Update histogram-best

--- a1/histogram/histogram-best.cpp
+++ a1/histogram/histogram-best.cpp
@@ -23,6 +23,7 @@ struct histogram {
 	}
 
 	void print_total(std::ostream& str) {
+		// str << "total:" << accumulate(data.begin(), data.end(), 0) << "\n";
 		str << "total:" << accumulate(data.begin(), data.end(), 0) << "\n";
 	}
 
@@ -30,22 +31,23 @@ struct histogram {
 		for (size_t i = 0; i < data.size(); ++i) str << i << ":" << data[i] << "\n";
 	}
 
-void append(const histogram& other) {
-		if (data.size() != other.data.size()) {
+void append(const histogram& thread_h) {
+		if (data.size() != thread_h.data.size()) {
 				throw std::invalid_argument("Histograms must have the same size to append.");
 		}
 		for (size_t i = 0; i < data.size(); ++i) {
-				data[i] += other.data[i];
+				data[i] += thread_h.data[i];
 		}
   }
 	
 };
 
-void worker(int sample_count, histogram& h, int num_bins) 
+void worker(int sample_count, histogram& h, int num_bins)//, long& count) 
 {
 	long count = 0.0;
 
 	generator gen(num_bins);
+	// count = 0;
 
 	while (sample_count--) {
 		int next = gen();
@@ -62,20 +64,26 @@ int main(int argc, char **argv)
 
 	int num_threads = std::thread::hardware_concurrency();
 	int print_level = 3; // 0: exec info + histogram total, 1: + histogram bins, 2: +exec time, 3: +bin info
-	int sample_per_thread = sample_count/num_threads;
-	int remaining_samples = sample_count - sample_per_thread*(num_threads-1);
-	
+
 	parse_args(argc, argv, num_threads, num_bins, sample_count, print_level);
 	
+	// counting the number of samples that each thread should compute
+	int sample_per_thread = sample_count/num_threads;
+	// counting the remaining samples for the last thread
+	int remaining_samples = sample_count - sample_per_thread*(num_threads-1);
+
 	histogram h(num_bins);
+	// creating a vector of histograms based on the number of threads
   vector<histogram> hists(num_threads, histogram(num_bins));
 
 	auto t1 = chrono::high_resolution_clock::now();
 
-	for (int i=0; i<num_threads-i; i++){
+	// creating num_threads-1 threads which call the worker function with sample_per_thread
+	for (int i=0; i<num_threads-1; i++){
 		threads.push_back(thread(worker, sample_per_thread, std::ref(hists[i]), num_bins));
 	}
 
+	// creating the last thread which calls 
 	threads.push_back(thread(worker, remaining_samples, std::ref(hists[num_threads-1]), num_bins));
 
 	for(auto& thread : threads) {
@@ -85,9 +93,11 @@ int main(int argc, char **argv)
 	for (const auto& h_th : hists) {
 			h.append(h_th);
 	}
-	// worker(sample_count, h, num_bins);
+
 
 	auto t2 = chrono::high_resolution_clock::now();
+
+	cout << (num_threads-1)*sample_per_thread + remaining_samples << endl;
 
 	if ( print_level >= 0 ) cout << "Bins: " << num_bins << ", sample size: " << sample_count << ", threads: " << num_threads << endl;
 	if ( print_level >= 3 ) h.print_bins(cout);
